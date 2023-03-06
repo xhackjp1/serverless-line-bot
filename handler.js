9c6dbc33c59c27664b48d616d23d8f5dc84eb227
shinsukematsuda <smatsuda@x-hack.jp>
2023-03-07 01:21:07 +0900

    Node v16対応

--- handler.js
+++ handler.js
@@ -1,8 +1,9 @@
-'use strict';
+"use strict";
 
-const line = require('@line/bot-sdk');
-const crypto = require('crypto');
-const request = require('request');
+const line = require("@line/bot-sdk");
+const crypto = require("crypto");
+const request = require("request");
+const Log = require("@dazn/lambda-powertools-logger");
 
 const client = new line.Client({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -12,56 +13,69 @@ const client = new line.Client({
 function generateResponse(statusCode, lineStatus, message) {
   return {
     statusCode: statusCode,
-    headers: { 'X-Line-Status': lineStatus },
+    headers: { "X-Line-Status": lineStatus },
     body: `{"result":"${message}"}`,
   };
 }
 
-function validateSignature(event) {
-  const signature = crypto.createHmac('sha256', process.env.CHANNEL_SECRET).update(event.body).digest('base64');
-  const checkHeader = (event.headers || {})['X-Line-Signature'];
-  return signature === checkHeader;
-}
+const validateSignature = (event) => {
+  const signature = event.headers["X-Line-Signature"];
+  const body = event.body;
+  const hash = crypto
+    .createHmac("sha256", process.env.CHANNEL_SECRET)
+    .update(body)
+    .digest("base64");
+  return hash === signature;
+};
 
 function isLineConnectionError(replyToken, context) {
-  if (replyToken !== '00000000000000000000000000000000') return false;
+  if (replyToken !== "00000000000000000000000000000000") return false;
   // 接続確認エラー回避
-  context.succeed(generateResponse(200, 'OK', 'connect check'));
+  context.succeed(generateResponse(200, "OK", "connect check"));
   return true;
 }
 
-function getUserProfile(userId) {
-  return {
-    url: 'https://api.line.me/v2/bot/profile/' + userId,
-    json: true,
-    headers: {
-      'Authorization': 'Bearer {' + process.env.CHANNEL_ACCESS_TOKEN + '}',
-    },
-  };
+async function getUserProfile(userId) {
+  try {
+    const profile = await client.getProfile(userId);
+    return profile;
+  } catch (error) {
+    console.error(`Get profile error: ${error}`);
+    return null;
+  }
 }
 
-module.exports.callback = (event, context) => {
+async function replyMessage(replyToken, message) {
+  try {
+    const result = await client.replyMessage(replyToken, message);
+    return result;
+  } catch (error) {
+    console.error(`Get profile error: ${error}`);
+    return null;
+  }
+}
+
+module.exports.callback = async (event, context) => {
   const body = JSON.parse(event.body);
+
+  Log.info("検証", { event });
+
   const userId = body.events[0].source.userId;
   const text = body.events[0].message.text;
   const replyToken = body.events[0].replyToken;
 
-  if (!validateSignature(event)) return;
-  if (isLineConnectionError(replyToken, context)) return;
+  // if (!validateSignature(event)) return;
+  // if (isLineConnectionError(replyToken, context)) return;
 
-  request.get(getUserProfile(userId), function (error, response, body) {
-    if (error || response.statusCode != 200) return;
+  const userProfile = await getUserProfile(userId);
+  Log.info("ユーザープロフィール", { userProfile });
 
-    const message = {
-      'type': 'text',
-      'text': 'hello!!' + body.displayName + 'さん\n' + text,
-    };
+  const message = {
+    type: "text",
+    text: "hello!!" + userProfile.displayName + "さん\n" + text,
+  };
+  Log.info("メッセージデータ", { data: message });
 
-    client.replyMessage(replyToken, message)
-      // eslint-disable-next-line no-unused-vars
-      .then((response) => {
-        context.succeed(generateResponse(200, 'OK', 'complete'));
-      })
-      .catch((err) => console.log(err));
-  });
+  const messageResult = await replyMessage(replyToken, message);
+  Log.info("送信結果", { data: messageResult });
 };
